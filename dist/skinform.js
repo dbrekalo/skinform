32835c7b84e46d66b4860001afe1c6c3e74900c2
Damir Brekalo <damir.brekalo@trikoder.net>
2014-04-14 21:32:32 +0200

    Refactor, camelCasing, indentation change

--- dist/skinform.js
+++ dist/skinform.js
@@ -1,298 +1,286 @@
-// Skinform plugin -- Damir Brekalo
+;(function($, window){
 
-;(function ( $, window, document ) {
+	"use strict";
 
-    "use strict";
+	var $window = window.app && window.app.$window || $(window);
 
-    var $window = $(window),
-        $document = $(document);
+	function resolveType(element, options){
 
-    function ResolveType( element, options ){
+		if ($.data(element, 'skinform')) { return; }
 
-        if( $.data(element, 'skinform') ) { return; }
+		var $el = $(element), data;
 
-        var $el = $(element);
-        var defaults = $.skinform.defaults;
-        var data = null;
+		if ($el.is('select')) { data = new Skinselect(element, options && options.select); }
+		else if ($el.is('input[type="checkbox"]')) { data = new Skincheckbox(element, options && options.checkbox); }
+		else if ($el.is('input[type="radio"]')) { data = new Skinradio(element, options && options.radio); }
+		else if ($el.is('input[type="file"]')){ data = new Skinfile( element, options && options.file); }
 
-        if ( $el.is('select') ) {
+		if (data) { $.data(element, 'skinform', data); }
 
-            data = new Skinselect( element, defaults.select, options && options.select ? options.select : {} );
+	}
 
-        } else if ( $el.is('input[type="checkbox"]') ) {
+	// SKIN SELECT
+	// ****************************************************************
 
-            data = new Skincheckbox( element, defaults.checkbox, options && options.checkbox ? options.checkbox : {} );
+	function Skinselect(element, options){
 
-        } else if ( $el.is('input[type="radio"]') ){
+		this.options = $.extend({}, $.skinform.defaults.select, options);
+		this.$select = $(element);
+		this.init();
 
-            data = new Skinradio( element, defaults.radio, options && options.radio ? options.radio : {} );
+	}
 
-        } else if ( $el.is('input[type="file"]') ){
+	Skinselect.prototype = {
 
-            data = new Skinfile( element, defaults.file, options && options.file ? options.file : {} );
+		init: function(){
 
-        }
+			this.$el = this.$select.wrap('<div class="'+ this.options.skinClass +'" />').parent();
+			this.$btn = $('<span>').addClass(this.options.btnClass).text(this.$select.find('option:selected').text()).appendTo(this.$el);
 
-        if ( data ) { $.data( element, 'skinform', data);  }
+			if (this.$select.data('class')) { this.$el.addClass( this.$select.data('class') ); }
+			if (this.$select.data('btn-class')) { this.$btn.addClass( this.$select.data('btn-class') ); }
 
-    }
+			this.events();
 
+		},
 
-    // SKIN FILE
-    // ****************************************************************
+		events: function(){
 
-    function Skinfile( element, defaults, options ){
+			var self = this;
 
-        this.options = $.extend( {}, defaults, options);
-        this.input = element;
-        this.init();
+			this.$select.on({
+				'focus': function(){ self.$el.addClass( self.options.focusClass ); },
+				'blur': function(){ self.$el.removeClass( self.options.focusClass ); },
+				'change': function(){ self.$btn.text( self.$select.find('option:selected').text() ); }
+			});
 
-    }
+		}
+	};
 
-    Skinfile.prototype = {
+	// SKIN CHECKBOX
+	// ****************************************************************
 
-        init: function(){
+	function Skincheckbox(element, options){
 
-            this.$input = $( this.input );
-            this.$el = $('<div class="'+ this.options.skin_class +'">');
-            this.$label = $('<div class="'+ this.options.label_class +'">'+ this.options.label_text +'</div>').appendTo(this.$el);
-            this.$btn = $('<div class="'+ this.options.btn_class +'">'+ this.options.btn_text +'</div>').appendTo(this.$el);
+		this.options = $.extend({}, $.skinform.defaults.checkbox, options);
+		this.$input = $(element);
+		this.init();
 
-            this.$el.insertAfter( this.$input );
-            this.$input.appendTo( this.$el );
+	}
 
-            this.setup_events();
+	Skincheckbox.prototype = {
 
-        },
+		init: function(){
 
-        setup_events: function(){
+			this.$el = this.$input.parent();
 
-            var self = this;
+			if ( !this.$el.is('label') ) {
+				this.$el = this.$input.wrap('<label class="'+ this.options.skinClass +'"></label>').parent();
+				this.$el.prepend( this.$input.attr('value') );
+			} else {
+				this.$el.addClass( this.options.skinClass );
+			}
 
-            this.$input.on({
+			if ( this.$input.prop("checked") ) { this.$el.addClass( this.options.toggleClass ); }
+			if ( this.$input.data('class') ) { this.$el.addClass( this.$input.data('class') ); }
 
-                'focus': function(){ self.$input.data('val', fileInput.val()); },
-                'blur': function(){ self.$input.trigger('check_change'); },
-                'check_change': function(){ if(self.$input.val() && self.$input.val() != self.$input.data('val')){ self.$input.trigger('change'); }},
-                'change': function(){
+			this.events();
 
-                    var fileName = self.$input.val().split(/\\/).pop(),
-                        fileExt = 'skin_file_ext_' + fileName.split('.').pop().toLowerCase();
+		},
 
-                    self.$label.text(fileName).removeClass( self.$label.data('fileExt') || '').addClass(fileExt).data('fileExt', fileExt);
-                    self.$btn.text( self.options.btn_change_text );
+		events: function(){
 
-                },
-                click: function(){
+			var self = this;
 
-                    self.$input.data('val', self.$input.val());
-                    setTimeout(function(){ self.$input.trigger('check_change'); },100);
+			self.$input.on({
+				'change': function(){ self.$el.toggleClass( self.options.toggleClass ); },
+				'focus': function(){ self.$el.addClass( self.options.focusClass ); },
+				'blur': function(){ self.$el.removeClass( self.options.focusClass ); }
+			});
 
-                }
+		}
 
-            });
+	};
 
-            this.$el.on('mousemove', function(e){
+	// SKIN FILE
+	// ****************************************************************
 
-                var map = {
-                    'left': e.pageX - self.$el.offset().left - self.$input.outerWidth() + 20, //position right side 20px right of cursor X)
-                    'top': e.pageY - self.$el.offset().top - $window.scrollTop() - 3
-                };
+	function Skinfile(element, options){
 
-                if ( self.options.fixed_position ){ map.top = e.pageY - self.$el.offset().top - 3; }
+		this.options = $.extend({}, $.skinform.defaults.file, options);
+		this.input = element;
+		this.init();
 
-                self.$input.css( map );
+	}
 
-            });
+	Skinfile.prototype = {
 
-        }
-    };
+		init: function(){
 
+			this.$input = $( this.input );
+			this.$el = $('<div class="'+ this.options.skinClass +'">');
+			this.$label = $('<div class="'+ this.options.labelClass +'">'+ this.options.labelText +'</div>').appendTo(this.$el);
+			this.$btn = $('<div class="'+ this.options.btnClass +'">'+ this.options.btnText +'</div>').appendTo(this.$el);
 
-    // SKIN RADIO
-    // ****************************************************************
+			this.$el.insertAfter(this.$input);
+			this.$input.appendTo(this.$el);
 
-    function Skinradio( element, defaults, options ) {
+			this.events();
 
-        this.options = $.extend( {}, defaults, options);
-        this.input = element;
-        this.init();
+		},
 
-    }
+		events: function(){
 
-    Skinradio.prototype = {
+			var self = this;
 
-        init: function(){
+			this.$input.on({
 
-            var skinobj = this;
-            this.$input = $( this.input );
-            var input_name = this.input.name;
+				'focus': function(){ self.$input.data('val', this.$input.val()); },
+				'blur': function(){ self.$input.trigger('checkChange'); },
+				'checkChange': function(){
 
-            if( $.data(this.input, 'skinform_'+ input_name) ) { return; }
+					var inputVal = self.$input.val();
 
-            this.$wrap = this.options.group_selector ? this.$input.closest( this.options.group_selector ) : this.$input.closest('form');
-            if ( !this.$wrap.length ) { this.$wrap = $('body'); }
+					if(inputVal && inputVal != self.$input.data('val')) {
+						self.$input.trigger('change');
+					}
 
-            this.$elems = this.$wrap.find('input[name="'+ input_name +'"]');
-            this.$labels = $();
+				},
+				'change': function(){
 
-            this.$elems.each(function(){
+					var fileName = self.$input.val().split(/\\/).pop(),
+						fileExtClass = 'skinfileExt-' + fileName.split('.').pop().toLowerCase();
 
-                var input = this,
-                    $input = $(this),
-                    $el = $input.parent();
+					self.$label.text(fileName).removeClass( self.$label.data('fileExtClass') || '').addClass(fileExtClass).data('fileExtClass', fileExtClass);
+					self.$btn.text( self.options.btnChangeText );
 
-                if ( !$el.is('label') ) {
-                    $el = $input.wrap('<label class="'+ skinobj.options.skin_class +'"></label>').parent();
-                    $el.prepend( input.value );
-                } else {
-                    $el.addClass( skinobj.options.skin_class );
-                }
+				},
+				click: function(){
 
-                if ( $input.prop("checked") ){ $el.addClass( skinobj.options.toggle_class ); }
+					self.$input.data('val', self.$input.val());
+					setTimeout(function(){ self.$input.trigger('checkChange'); },100);
 
-                skinobj.$labels = skinobj.$labels.add( $el );
-                $.data(input, 'skinform_'+ input_name, skinobj);
+				}
 
-            });
+			});
 
-            this.setup_events();
+			this.$el.on('mousemove', function(e){
 
+				var map = {
+					'left': e.pageX - self.$el.offset().left - self.$input.outerWidth() + 20, //position right side 20px right of cursor X)
+					'top': e.pageY - self.$el.offset().top - $window.scrollTop() - 3
+				};
 
-        },
+				if ( self.options.fixedPosition ){ map.top = e.pageY - self.$el.offset().top - 3; }
 
-        setup_events: function(){
+				self.$input.css( map );
 
-            var self = this;
+			});
 
-            this.$elems.on({
-                change: function(){ self.$labels.removeClass( self.options.toggle_class ); $(this).parent().addClass( self.options.toggle_class ); },
-                focus: function(){ self.$labels.removeClass( self.options.focus_class ); $(this).parent().addClass( self.options.focus_class ); },
-                blur: function(){ $(this).parent().removeClass( self.options.focus_class ); }
-            });
+		}
+	};
 
-        }
 
-    };
+	// SKIN RADIO
+	// ****************************************************************
 
-    // SKIN CHECKBOX
-    // ****************************************************************
+	function Skinradio(element, options){
 
-    function Skincheckbox( element, defaults, options ) {
+		this.options = $.extend({}, $.skinform.defaults.radio, options);
+		this.input = element;
+		this.init();
 
-        this.options = $.extend( {}, defaults, options);
-        this.$input = $(element);
-        this.init();
+	}
 
-    }
+	Skinradio.prototype = {
 
-    Skincheckbox.prototype = {
+		init: function(){
 
-        init: function(){
+			var self = this;
+			this.$input = $( this.input );
+			var inputName = this.input.name;
 
-            this.$el = this.$input.parent();
+			if($.data(this.input, 'skinform-'+ inputName)) { return; }
 
-            if ( !this.$el.is('label') ) {
-                this.$el = this.$input.wrap('<label class="'+ this.options.skin_class +'"></label>').parent();
-                this.$el.prepend( this.$input.attr('value') );
-            } else {
-                this.$el.addClass( this.options.skin_class );
-            }
+			this.$wrap = this.options.groupSelector ? this.$input.closest( this.options.groupSelector ) : this.$input.closest('form');
+			if (!this.$wrap.length) { this.$wrap = $('body'); }
 
-            if ( this.$input.prop("checked") ) { this.$el.addClass( this.options.toggle_class ); }
-            if ( this.$input.data('class') ) { this.$el.addClass( this.$input.data('class') ); }
+			this.$elems = this.$wrap.find('input[name="'+ inputName +'"]');
+			this.$labels = $();
 
-            this.setup_events();
+			this.$elems.each(function(){
 
-        },
+				var input = this,
+					$input = $(this),
+					$el = $input.parent();
 
-        setup_events: function(){
+				if (!$el.is('label')) {
+					$el = $input.wrap('<label class="'+ self.options.skinClass +'"></label>').parent();
+					$el.prepend( input.value );
+				} else {
+					$el.addClass( self.options.skinClass );
+				}
 
-            var self = this;
+				if ($input.prop("checked")){ $el.addClass( self.options.toggleClass ); }
 
-            self.$input.on({
-                'change': function(){ self.$el.toggleClass( self.options.toggle_class ); },
-                'focus': function(){ self.$el.addClass( self.options.focus_class ); },
-                'blur': function(){ self.$el.removeClass( self.options.focus_class ); }
-            });
+				self.$labels = self.$labels.add( $el );
+				$.data(input, 'skinform-'+ inputName, self);
 
-        }
+			});
 
-    };
+			this.events();
 
 
-    // SKIN SELECT
-    // ****************************************************************
+		},
 
-    function Skinselect( element, defaults, options ) {
+		events: function(){
 
-        this.options = $.extend( {}, defaults, options);
-        this.$select = $(element);
-        this.init();
+			var self = this;
 
-    }
+			this.$elems.on({
+				change: function(){ self.$labels.removeClass( self.options.toggleClass ); $(this).parent().addClass( self.options.toggleClass ); },
+				focus: function(){ self.$labels.removeClass( self.options.focusClass ); $(this).parent().addClass( self.options.focusClass ); },
+				blur: function(){ $(this).parent().removeClass( self.options.focusClass ); }
+			});
 
-    Skinselect.prototype = {
+		}
 
-        init: function(){
+	};
 
-            this.$el = this.$select.wrap('<div class="'+ this.options.skin_class +'" />').parent();
-            this.$btn = $('<span class="'+ this.options.btn_class +'">'+ this.$select.find('option:selected').text()+'</span>');
-            this.$el.prepend( this.$btn );
+	// ...AND GO!
+	// ****************************************************************
 
-            if ( this.$select.data('class') ) { this.$el.addClass( this.$select.data('class') ); }
-            if ( this.$select.data('btn-class') ) { this.$btn.addClass( this.$select.data('btn-class') ); }
+	$.skinform = {};
 
-            this.setup_events();
+	$.fn.skinform = function( options ){
+		return this.each(function(){ resolveType(this, options); });
+	};
 
-        },
+	$.skinform.defaults = {
+		select: {
+			skinClass: 'skin_select',
+			btnClass: 'btn',
+			focusClass: 'focused'
+		},
+		checkbox: {
+			skinClass: 'skin_checkbox',
+			toggleClass: 'on',
+			focusClass: 'focused'
+		},
+		radio: {
+			skinClass: 'skin_radio',
+			toggleClass: 'on',
+			focusClass: 'focused',
+			groupSelector: null
+		},
+		file: {
+			skinClass: 'skin_file',
+			btnClass: 'skin_file_btn',
+			labelClass: 'skin_file_label',
+			btnText: 'Odaberi',
+			btnChangeText: 'Promijeni',
+			labelText: 'Odaberite datoteku'
+		}
+	};
 
-        setup_events: function(){
-
-            var self = this;
-            this.$select.on({
-                'focus': function(){ self.$el.addClass( self.options.focus_class ); },
-                'blur': function(){ self.$el.removeClass( self.options.focus_class ); },
-                'change': function(){ self.$btn.text( self.$select.find('option:selected').text() ); }
-            });
-
-        }
-    };
-
-    // ...AND GO!
-    // ****************************************************************
-
-    $.skinform = {};
-
-    $.fn.skinform = function( options ){
-        return this.each(function(){ ResolveType(this, options); });
-    };
-
-    $.skinform.defaults = {
-        select: {
-            skin_class: 'skin_select',
-            btn_class: 'btn',
-            focus_class: 'focused'
-        },
-        checkbox: {
-            skin_class: 'skin_checkbox',
-            toggle_class: 'on',
-            focus_class: 'focused'
-        },
-        radio: {
-            skin_class: 'skin_radio',
-            toggle_class: 'on',
-            focus_class: 'focused',
-            group_selector: null
-        },
-        file: {
-            skin_class: 'skin_file',
-            btn_class: 'skin_file_btn',
-            label_class: "skin_file_label",
-            btn_text: 'Odaberi',
-            btn_change_text: 'Promijeni',
-            label_text: 'Odaberite datoteku'
-        }
-    };
-
-})( jQuery, window, document );
+})( window.jQuery || window.Zepto, window );
